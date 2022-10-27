a9a5ce305ea928904c2aa98f215ab94b07e0e467
Nídio Dolfini <nidiosdolfini@gmail.com>
2022-10-26 22:05:44 -0300

    update terraform

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
 provider "aws" {
-  region = "sa-east-1"
+  region = var.regiao
 }
-
-resource "aws_instance" "nidio_ec2" {
-    count = 2
-  ami           = "ami-04b3c23ec8efcc2d6"
-  instance_type = "t2.micro"
-  key_name      = "nidio-terraform-aws"
-  tags = {
-    "Name" = "nidio-ec2_${count.index}"
-  }
-}
-
-resource "aws_key_pair" "chave_terraform_aws" {
-  key_name   = "nidio-terraform-aws"
-  public_key = file("terraform-aws.pub")
-}
-
