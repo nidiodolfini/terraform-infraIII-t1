6903f0736a4a35d9e2f875ea9d9bd322ed0e85ee
Nídio Dolfini <nidiosdolfini@gmail.com>
2022-11-01 15:58:21 -0300

    update ansible

--- output.tf
+++ output.tf
@@ -1,3 +1,10 @@
 output "instancia_ec2" {
   value = aws_instance.nidio_ec2.*.public_ip
 }
+output "instancia_ec2_private" {
+  value = aws_instance.nidio_ec2.*.private_ip
+}
+
+output "instancia_ec2_ansible" {
+  value = aws_instance.nidio_ec2_ansible.*.public_ip
+}
